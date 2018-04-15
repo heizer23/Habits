9e5e2b812a2642198f547aee52fa0710135c68f0
heizer23 <p.remmecke@gmail.com>
2018-04-15 18:52:33 +0200

    Alles neu analog zum Tutorial

---
+++ app/src/main/java/de/remmecke/android/habits/data/HabitPreferences.java
@@ -0,0 +1,18 @@
+package de.remmecke.android.habits.data;
+
+import android.content.Context;
+
+/**
+ * Created by Linse on 15.04.2018.
+ * Keine Ahnung was hier rein soll :/
+ */
+
+public class HabitPreferences {
+
+    /*
+     * Human readable location string, provided by the API.  Because for styling,
+     * "Mountain View" is more recognizable than 94043.
+     */
+    public static final String PREF_CITY_NAME = "city_name";
+
+}
